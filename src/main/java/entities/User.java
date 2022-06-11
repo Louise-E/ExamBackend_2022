a32a807a9568c20ae71d68bff2646ff6ed21883c
Louise-E <cph-le115@cphbusiness.dk>
2022-06-11 15:39:20 +0200

    user changes

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -10,7 +10,7 @@ import org.mindrot.jbcrypt.BCrypt;
 
 @Entity
 @Table(name = "user")
-//@NamedQuery(name = "Users.deleteAllRows", query = "DELETE FROM Users")
+@NamedQuery(name = "Users.deleteAllRows", query = "DELETE FROM User")
 public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
